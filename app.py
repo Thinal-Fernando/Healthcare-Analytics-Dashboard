b036a112c91dcc8c24dfe91405d80f4c585b313c
Thinal <r.thinal.fernando@gmail.com>
2025-09-11 18:42:51 +0530

    - Imported necessary packages: dash, pandas, dash_bootstrap_components, plotly.express - Created `load_data()` function to read CSV and clean columns:   - Converted 'Billing Amount' to numeric   - Converted 'Date of Admission' to datetime - Loaded dataset into `data`

--- app.py
+++ app.py
@@ -1,4 +1,20 @@
 import dash
+import pandas as pd
+import dash_bootstrap_components as dbc
+from dash import dcc, Input, Output, html
+import plotly.express as px
+
+
+def load_data():
+    df = pd.read_csv('assets/healthcare.csv')    # Loading the dataset
+    df["Billing Amount"] = pd.to_numeric(df['Billing Amount'])    # converting billing amount to a numeric value
+    df["Date of Admission"] = pd.to_datetime(df['Date of Admission'])    # Convert 'Date of Admission' into proper datetime
+    return df
+
+data = load_data()
+
+
+
 
 
 app = dash.Dash(__name__)
